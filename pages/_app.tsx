27154cc2ca7e7a2e27f576a71f3a0761b0d21531
Peter Yoo <trustintofu@gmail.com>
2022-10-29 23:41:08 -0700

    feat: Wraps our app with react query QueryClientProvider and our Layout component.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Layout } from '../components/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <div className="p-10 mx-auto max-w-6xl">
+          <Component {...pageProps} />
+        </div>
+      </Layout>
+    </QueryClientProvider>
+  );
 }
