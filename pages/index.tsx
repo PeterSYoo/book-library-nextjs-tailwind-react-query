f78ff8b7a8ae3efd464dc6382e8d852f54b9a75f
Peter Yoo <trustintofu@gmail.com>
2022-10-29 23:44:57 -0700

    feat: Implements useQuery to fetch our books data and caches it in a books state. Map the returned data from useQuery and display book author and title.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import { useQuery } from '@tanstack/react-query';
+import { getAllBooks } from '../lib/booksHelper';
 
 export default function Home() {
+  const { data, error, isLoading, isError }: any = useQuery(
+    ['books'],
+    getAllBooks
+  );
+
+  if (isLoading) {
+    return <div>Loading..</div>;
+  }
+
+  if (isError) {
+    return <div>Error {error.message}</div>;
+  }
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="">
+        {data.map(({ author, title, id }: any) => (
+          <div key={id}>
+            {author} - {title}
+          </div>
+        ))}
+      </div>
     </>
   );
 }
