3b066c641e8e53c100e2e7c853ca9d90d4b45b5b
Peter Yoo <trustintofu@gmail.com>
2022-10-30 18:23:51 -0700

    feat: Updates CRUD helper functions.

--- lib/booksHelper.ts
+++ lib/booksHelper.ts
@@ -9,3 +9,73 @@ export const getAllBooks = async () => {
     return response.json();
   }
 };
+
+export const getBook = async ({ queryKey }: any) => {
+  const [_key, { id }] = queryKey;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_API_SERVER}/books/${id}`
+  );
+
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error(response.json().message);
+  } else {
+    return response.json();
+  }
+};
+
+export const updateBook = async ({ id, ...data }: any) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_API_SERVER}/books/${id}`,
+    {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }
+  );
+
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error(response.json().message);
+  } else {
+    return true;
+  }
+};
+
+export const removeBook = async (id: any) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_API_SERVER}/books/${id}`,
+    {
+      method: 'DELETE',
+    }
+  );
+
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error(response.json().message);
+  } else {
+    return true;
+  }
+};
+
+export const createBook = async (data: any) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_API_SERVER}/books`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }
+  );
+
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error(response.json().message);
+  } else {
+    return response.json();
+  }
+};
