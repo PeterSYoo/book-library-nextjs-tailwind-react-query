d0ef3491cad042d34d09bd2ae14d47c0003281cb
Peter Yoo <trustintofu@gmail.com>
2022-10-30 20:49:30 -0700

    feat: Adds Title and Author line.

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,10 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col gap-5">
+        <div className="flex justify-between pr-28 pb-2 border-b border-gray-300">
+          <span className="text-bold">Title</span>
+          <span className="text-bold">Author</span>
+        </div>
         {data?.map(({ author, title, id }: any) => (
           <BookItem author={author} title={title} key={id} id={id} />
         ))}
