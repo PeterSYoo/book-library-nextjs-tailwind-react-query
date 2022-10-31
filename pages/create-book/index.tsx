ec8f9cd0dd1f40176fb47cfe6153c8a13c8cf2d2
Peter Yoo <trustintofu@gmail.com>
2022-10-30 18:28:11 -0700

    feat: Creates book creation page, uses useMutation with create helper function to post new data to our database.

--- pages/create-book/index.tsx
+++ pages/create-book/index.tsx
@@ -1,5 +1,25 @@
+import { QueryClient, useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
+import { BookForm } from '../../components/book-form.components';
+import { createBook } from '../../lib/booksHelper';
+
 const CreateBook = () => {
-  return <div>CreateBook</div>;
+  const router = useRouter();
+  const { mutateAsync, isLoading } = useMutation(createBook);
+
+  const onFormSubmit = async (data: any) => {
+    await mutateAsync(data);
+    router.push('/');
+  };
+
+  return (
+    <>
+      <div className="flex flex-col gap-3">
+        <h1>Create New Book</h1>
+        <BookForm onFormSubmit={onFormSubmit} isLoading={isLoading} />
+      </div>
+    </>
+  );
 };
 
 export default CreateBook;
